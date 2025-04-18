12091375b85ca18610ed9cafb7d8038d7ddb1fde
Vladimir Korencik <vkorencik@typesoft.io>
2025-04-18 17:02:23 +0200

    first commit

---
+++ svelte.config.js
@@ -0,0 +1,18 @@
+import adapter from '@sveltejs/adapter-node';
+import { sveltePreprocess } from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: sveltePreprocess(),
+
+  kit: {
+    csrf: {
+      checkOrigin: false,
+    },
+    adapter: adapter(),
+  }
+};
+
+export default config;
